261ef40e197276c855e7a6a8cd433e2e553d268f
Simon Neidig <simon1234@online.de>
2024-06-02 16:58:42 +0200

    Implement profile picture in sidebar

--- src/components/sidebar/personalInfo/personalInfo.tsx
+++ src/components/sidebar/personalInfo/personalInfo.tsx
@@ -1,5 +1,32 @@
- 
-  export default function PersonalInfo() {
-    return <div className="flex justify-center gap-2"></div>;
-  }
-  
+import {
+  ChartBarSquareIcon,
+  EnvelopeIcon,
+  MapPinIcon,
+} from "@heroicons/react/24/outline";
+import PersonalInfoItem from "@/components/sidebar/personalInfo/personalInfoItem";
+
+export default function PersonalInfo() {
+  return (
+    <div className="flex bg-light-grey justify-center mt-6 mx-10 rounded-xl">
+      <div className="m-5 w-full">
+        <PersonalInfoItem
+          label="Email"
+          value="mail@simonneidig.de"
+          icon={<EnvelopeIcon />}
+        />
+        <div className="bg-white h-[1px] mx-10" />
+        <PersonalInfoItem
+          label="Location"
+          value="Germany"
+          icon={<MapPinIcon />}
+        />
+        <div className="bg-white h-[1px] mx-10" />
+        <PersonalInfoItem
+          label="Company"
+          value="SCHUMANN"
+          icon={<ChartBarSquareIcon />}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/sidebar/personalInfo/personalInfoItem.tsx
+++ src/components/sidebar/personalInfo/personalInfoItem.tsx
@@ -1,7 +1,9 @@
+import Badge from "@/components/general/badge";
+
 interface PersonalInfoItemObject {
   label: string;
   value: string;
-  icon: string;
+  icon: React.ReactNode;
 }
 
 export default function PersonalInfoItem({
@@ -9,5 +11,19 @@ export default function PersonalInfoItem({
   value,
   icon,
 }: PersonalInfoItemObject) {
-  return <div className="flex justify-center gap-2"></div>;
+  return (
+    <div className="grid grid-cols-4 w-full my-1 gap-2">
+      <div className="grid col-span-1 items-center justify-items-center ">
+        <div className=" bg-white text-primary rounded-lg drop-shadow-xl">
+          <div className="m-3">
+            <div className="size-7">{icon}</div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-3 my-2">
+        <div className="text-dark-grey col-span-4 text-sm">{label}</div>
+        <div className="">{value}</div>
+      </div>
+    </div>
+  );
 }
