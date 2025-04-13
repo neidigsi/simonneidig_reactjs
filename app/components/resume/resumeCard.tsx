1f3ffecb4de3ef1fdbda7b0e14ef480df9149af2
Simon Neidig <simon1234@online.de>
2025-04-13 16:32:27 +0200

    Finalize translations

--- app/components/resume/resumeCard.tsx
+++ app/components/resume/resumeCard.tsx
@@ -1,11 +1,16 @@
+// Import external dependencies
+import { useTranslation } from 'react-i18next';
+
 // Import internal dependencies
 import Card from "@/components/general/card";
 import EducationList from "./education/educationList";
 import ExperienceList from "./experience/experienceList";
 
 export default function ResumeCard() {
+  const { t } = useTranslation();
+
   return (
-    <Card headline="Resume">
+    <Card headline={t("main.resume.title")}>
       <div className="text-base grid grid-cols-1 lg:grid-cols-2  gap-4">
         <div className="col-span-1">
           <ExperienceList />
