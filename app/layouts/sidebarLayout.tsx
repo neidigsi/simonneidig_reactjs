28fcfa3c91ba8315be4ecfdd20cc96e5ebab919c
Simon Neidig <simon1234@online.de>
2025-05-25 16:58:31 +0200

    Load all relevant infos for sidebar inside of sidebar layout

--- app/layouts/sidebarLayout.tsx
+++ app/layouts/sidebarLayout.tsx
@@ -5,11 +5,15 @@ import { useLocation } from "react-router";
 
 // Import internal dependencies
 import Sidebar from "@/components/sidebar/sidebar";
-import { useAppDispatch } from "@/store/hooks";
+import { useAppSelector, useAppDispatch } from "@/store/hooks";
 import Navigation from "@/components/navigation/navigation";
 import DarkModeToggl from "@/components/actionBar/darkModeToggl";
 import LanguageSwitcher from "@/components/actionBar/languageSwitcher";
 import { changeLanguage } from "@/store/slices/settingsSlice";
+import { loadPersonalDetails } from "@/store/slices/personalDetailsSlice";
+import { loadPersonalInfo } from "@/store/slices/personalInfoSlice";
+import { loadSocialMedia } from "@/store/slices/socialMediaSlice";
+import Loader from "@/components/general/loader/loader";
 
 export default function SidebarLayout({
   children,
@@ -20,6 +24,25 @@ export default function SidebarLayout({
   const navRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
   const prevPathRef = useRef(location.pathname);
+  const personalDetailsLoaded = useAppSelector(
+    (state) => state.personalDetails.loaded
+  );
+  const personalInfoLoaded = useAppSelector(
+    (state) => state.personalInfo.loaded
+  );
+  const socialMediaLoaded = useAppSelector((state) => state.socialMedia.loaded);
+
+  useEffect(() => {
+    if (!personalDetailsLoaded) {
+      dispatch(loadPersonalDetails({ language: i18n.language }));
+    }
+    if (!personalInfoLoaded) {
+      dispatch(loadPersonalInfo({ language: i18n.language }));
+    }
+    if (!socialMediaLoaded) {
+      dispatch(loadSocialMedia({ language: i18n.language }));
+    }
+  });
 
   useEffect(() => {
     dispatch(changeLanguage(i18n.language));
@@ -44,26 +67,34 @@ export default function SidebarLayout({
   }, [location.pathname]);
 
   return (
-    <div className="max-w-[1350px] w-screen">
-      <div className="mx-8 grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-8 xl:grid-cols-7">
-        <div className="col-span-1 md:col-span-1 lg:col-span-3 xl:col-span-2">
-          <div className="md:hidden flex justify-end button-bar">
-            <LanguageSwitcher />
-            <DarkModeToggl />
+    <>
+      {personalDetailsLoaded && personalInfoLoaded && socialMediaLoaded ? (
+        <div className="max-w-[1350px] w-screen">
+          <div className="mx-8 grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-8 xl:grid-cols-7">
+            <div className="col-span-1 md:col-span-1 lg:col-span-3 xl:col-span-2">
+              <div className="md:hidden flex justify-end button-bar">
+                <LanguageSwitcher />
+                <DarkModeToggl />
+              </div>
+              <Sidebar />
+            </div>
+            <div className="col-span-1 md:col-span-1 lg:col-span-5 xl:col-span-5">
+              <div className="hidden md:flex justify-end button-bar">
+                <LanguageSwitcher />
+                <DarkModeToggl />
+              </div>
+              <div className="main-section">
+                <Navigation navRef={navRef} />
+                {children}
+              </div>
+            </div>
           </div>
-          <Sidebar />
         </div>
-        <div className="col-span-1 md:col-span-1 lg:col-span-5 xl:col-span-5">
-          <div className="hidden md:flex justify-end button-bar">
-            <LanguageSwitcher />
-            <DarkModeToggl />
-          </div>
-          <div className="main-section">
-            <Navigation navRef={navRef} />
-            {children}
-          </div>
+      ) : (
+        <div className="grid h-screen w-screen">
+          <Loader size={5} color="white" />
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
