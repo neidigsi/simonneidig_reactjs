a625e59100fb2c7a83bb78827f223c8db1dd4fea
Simon Neidig <simon1234@online.de>
2025-05-31 18:25:40 +0200

    Fix double import of react in vite app

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
-    plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
-    define: {
-      VITE_APP_BACKEND_ADDRESS: JSON.stringify(env.VITE_APP_BACKEND_ADDRESS),
-    },
-  };
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
