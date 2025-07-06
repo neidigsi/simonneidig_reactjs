dde0246c946612008f3ed396569e9e4b3367374c
Simon Neidig <simon1234@online.de>
2025-07-06 12:42:10 +0200

    Extend root

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 // Import external dependencies
-import { Outlet } from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
 // Import internal dependencies
 import type { Route } from "./+types/root";
@@ -11,6 +11,24 @@ export const links: Route.LinksFunction = () => [
     href: "/favicon.ico",
   },
 ];
+
+export function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
 export default function App() {
   return <Outlet />;
